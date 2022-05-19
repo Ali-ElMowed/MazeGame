a99839f340311e2080ecc02c34d0984ba3736fb2
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-20 00:52:17 +0300

    Adding Cheating function

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ window.onload = function () {
     reset.style.padding = "10px 20px";
     document.body.appendChild(reset);
     start.onclick = function () {
+        game.addEventListener("mouseleave", cheating)
         status.textContent = "You Can Do It!";
         for (var i = 0; i < boundries.length; i++) {
             boundries[i].style.backgroundColor = "#eeeeee";
@@ -37,6 +38,8 @@ window.onload = function () {
             boundries[i].style.backgroundColor = redBg;
             boundries[i].removeEventListener("mouseover", losing);
             end.removeEventListener("mouseover",wining);
+            game.removeEventListener("mouseleave", cheating)
+
         }
         score = score - 10;
         scoreDisplay.style.backgroundColor = "rgb(154 8 230)"
@@ -49,11 +52,16 @@ window.onload = function () {
             var greenBg = boundries[i].style.backgroundColor = "#18f407";
             boundries[i].style.backgroundColor = greenBg;
             boundries[i].removeEventListener("mouseover", losing);
+            game.removeEventListener("mouseleave", cheating)
+
         }
         score = score + 5;
         scoreDisplay.style.backgroundColor = "rgb(154 8 230)";
         scoreDisplay.textContent = "Score:  " + `${score}`;
         status.textContent = "You Win!";
+    } 
+    function cheating() {
+            alert("Don't Cheat!");
+            losing();
     }
-    
 }
